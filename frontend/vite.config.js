2b57ecc1fc380d6ffc56ca656390eaa12cf400cc
Subash S <subash.s2024aids@sece.ac.in>
2025-12-18 21:16:57 +0530

    Add files via upload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    copyPublicDir: true
+  },
+  publicDir: 'public'
 })
